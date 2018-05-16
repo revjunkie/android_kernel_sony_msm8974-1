8dd6d7d3bd5dd809926029db9891bf88fa9c210b
revjunkie <rajibrahim@rocketmail.com>
2018-05-16 22:12:46 +0800

    Revert "mdss: add backlight dimmer"
    This reverts commit 8adca4f9b9264cf820e8ad2bbfd2a8fc0da85ade.

--- drivers/video/msm/mdss/mdss_dsi_panel_driver.c
+++ drivers/video/msm/mdss/mdss_dsi_panel_driver.c
@@ -453,35 +453,6 @@ static void mdss_dsi_panel_switch_mode(struct mdss_panel_data *pdata,
 	return;
 }
 
-static int bl_dim = 0;
-static int bl_val[] = { 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 2, 3, 4, 5, 6, 7,
-		8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-		18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-		28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-		38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-		48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-		58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
-		68, 69, 70, 71, 72, 73, 74, 75, 76, 77,
-		78, 79, 80, 81, 82, 83, 84, 85, 86, 87,
-		88, 89, 90, 91, 92, 93, 94, 95, 96, 97,
-		98, 99, 100, 101, 102, 103, 104, 105, 106, 107,
-		108, 109, 110, 111, 112, 113, 114, 115, 116, 117,
-		118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-		128, 129, 130, 131, 132, 133, 134, 135, 136, 137,
-		138, 139, 140, 141, 142, 143, 144, 145, 146, 147,
-		148, 149, 150, 151, 152, 153, 154, 155, 156, 157,
-		158, 159, 160, 161, 162, 163, 164, 165, 166, 167,
-		168, 169, 170, 171, 172, 173, 174, 175, 176, 177,
-		178, 179, 180, 181, 182, 183, 184, 185, 186, 187,
-		188, 189, 190, 191, 192, 193, 194, 195, 196, 197,
-		198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
-		208, 209, 210, 211, 212, 213, 214, 215, 216, 217,
-		218, 219, 220, 221, 222, 223, 224, 225, 226, 227,
-		228, 229, 230, 231, 232, 233, 234, 235, 236, 237,
-		238, 239, 240, 242, 246, 248, 250, 255, 255, 255,
-};
-
 static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 							u32 bl_level)
 {
@@ -494,12 +465,6 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
-
-
-    if (bl_dim) {
-        if (bl_level < 256 && bl_level != 0)
-            bl_level = bl_val[bl_level];
-    }
 
 	/*
 	 * Some backlight controllers specify a minimum duty cycle
@@ -601,24 +566,6 @@ static ssize_t mdss_dsi_panel_vsyncs_per_ksecs_store(struct device *dev,
 	}
 exit:
 	return ret;
-}
-
-static ssize_t bl_dim_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	return scnprintf(buf, PAGE_SIZE, "%u\n", bl_dim);
-}
-
-static ssize_t bl_dim_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
-{
-	unsigned int input;
-	int ret;
-	ret = sscanf(buf, "%u", &input);
-	if (ret != 1)
-		return -EINVAL;
-	bl_dim = input;
-	return count;
 }
 
 static ssize_t mdss_dsi_panel_interval_ms_show(struct device *dev,
@@ -1088,8 +1035,6 @@ static struct device_attribute panel_attributes[] = {
 	__ATTR(change_fpks, S_IRUGO|S_IWUSR|S_IWGRP,
 					mdss_dsi_panel_change_fpks_show,
 					mdss_dsi_panel_change_fpks_store),
-    __ATTR(bl_dim, S_IRUGO|S_IWUSR|S_IWGRP, bl_dim_show,
-                    bl_dim_store),
 };
 
 static int register_attributes(struct device *dev)
